c4b895785caa67d7720804ec8b13f976d946cf2c
swleb <136448489+swleb@users.noreply.github.com>
2023-06-13 13:09:47 +0000

    updated color and name

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color:blueviolet"
+          Mili
         </b-navbar-item>
       </template>
     </b-navbar>
@@ -79,3 +79,4 @@ export default class Home extends Vue {}
   bottom: 0;
 }
 </style>
+
